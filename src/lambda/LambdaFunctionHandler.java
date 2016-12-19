7b32b1a350cf0d763f9db5d584734c2a88b6d058
dgallagher-ire <mobile2158857@gmail.com>
2016-12-19 05:51:57 +0000

    update

--- src/lambda/LambdaFunctionHandler.java
+++ src/lambda/LambdaFunctionHandler.java
@@ -3,6 +3,9 @@ package lambda;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
@@ -11,48 +14,45 @@ import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.S3Event;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.Bucket;
 import com.amazonaws.services.s3.model.GetObjectRequest;
 import com.amazonaws.services.s3.model.S3Object;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
-public class LambdaFunctionHandler implements RequestHandler<S3Event, Object> {
+import lambda.file.Record;
+import lambda.file.Records;
+
+public class LambdaFunctionHandler implements RequestHandler<RequestClass, ResponseClass> {
 
     @Override
-    public Object handleRequest(S3Event input, Context context) {
+    public ResponseClass handleRequest(RequestClass input, Context context) {
         context.getLogger().log("Input: " + input);
 
         // TODO: implement your handler
         return null;
     }
     
-    private void copyContentsToFile(final LambdaLogger logger,
-			final AmazonS3 s3Client,
-			final String bucketName,
-			final String key,
-			final String contents) throws Exception {
-		
-		try {
-			final String newFileName = key.replace(".dg", ".copy");
-			s3Client.putObject(bucketName, newFileName, contents);
-			logger.log("File contents copied to: "+newFileName);
-		} catch (Exception e) {
-			logger.log(e.toString());
-			throw e;
+    public static Records getRecords(final AmazonS3 s3Client) throws Exception {
+		final StringBuilder sb = new StringBuilder();
+		final String loaderJson = getFileContents(s3Client, "dgallagher-bucket", "loader-data.json");
+		if(loaderJson == null || loaderJson.equals("")){
+			return new Records();
 		}
+		final ObjectMapper mapper = new ObjectMapper();
+		final Records records = mapper.readValue(loaderJson, Records.class);
+		return records;
 	}
-    
-    private String readS3Contents(final LambdaLogger logger, final String bucketName, final String key)
+
+	private static String getFileContents(final AmazonS3 s3Client, final String bucketName, final String fileName)
 			throws Exception {
 
 		InputStream in = null;
 		try {
-			final AmazonS3 s3Client = new AmazonS3Client();
-			final GetObjectRequest objectRequest = new GetObjectRequest(bucketName, key);
-			final S3Object object = s3Client.getObject(objectRequest);
-			in = object.getObjectContent();
-			final InputStreamReader inReader = new InputStreamReader(in);
-			final BufferedReader reader = new BufferedReader(inReader);
+			final S3Object s3object = s3Client.getObject(new GetObjectRequest(bucketName, fileName));
+			in = s3object.getObjectContent();
+			final BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+			String line = null;
 			final StringBuilder sb = new StringBuilder();
-			String line;
 			while (true) {
 				line = reader.readLine();
 				if (line == null) {
@@ -62,11 +62,13 @@ public class LambdaFunctionHandler implements RequestHandler<S3Event, Object> {
 			}
 			return sb.toString();
 		} catch (Exception e) {
-			logger.log(e.toString());
 			throw e;
 		} finally {
 			if (in != null) {
-				in.close();
+				try {
+					in.close();
+				} catch (Exception e) {
+				}
 			}
 		}
 	}
